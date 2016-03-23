222c587a96eb401f4ac64a535fbdd0d305f142c2
luoa <luoa@uw.edu>
2016-03-22 23:52:25 -0700

    Changed idebar width slightly, added base code for flask-blog

--- launch.py
+++ launch.py
@@ -1,9 +1,48 @@
 import flask
 import random
+import sqlalchemy
 
-# Create the application.
+from flask.ext.login import UserMixin, LoginManager, login_user, logout_user
+from flask.ext.blogging import SQLAStorage, BloggingEngine
+
+#Create the application.
 APP = flask.Flask(__name__)
+APP.config["SECRET_KEY"] = "secret"  # for WTF-forms and login
+APP.config["BLOGGING_SITEURL"] = "http://127.0.0.1:5000"
 
+#extensions
+engine = sqlalchemy.create_engine('sqlite:////tmp/blog.db')
+meta = sqlalchemy.MetaData()
+
+sql_storage = SQLAStorage(engine, metadata=meta)
+blog_engine = BloggingEngine(APP, sql_storage)
+login_manager = LoginManager(APP)
+meta.create_all(bind=engine)
+
+#user class for providing authentication
+class User(UserMixin):
+    def __init__(self, user_id):
+        self.id = user_id
+
+    def get_name(self):
+        return id
+
+#for authentication
+@APP.route("/login/")
+def login():
+    user = User("testuser")
+    login_user(user)
+    return flask.redirect("/")
+
+@APP.route("/logout/")
+def logout():
+    logout_user()
+    return flask.redirect("/")
+
+@login_manager.user_loader
+@blog_engine.user_loader
+def load_user(user_id):
+    return User(user_id)
 
 #you can reuse templates with conditional statements!
 #given an arguement, can feed in various parameters to render certain parts of html!
@@ -29,7 +68,8 @@ def news():
 
 @APP.route("/blog")
 def blog():
-    return flask.render_template("aboutme.css", title = title())
+    return flask.render_template("blog.css", title = title())
+
 
 def title():
     titleName = "Andrew Luo"
@@ -40,4 +80,5 @@ def title():
 
 if __name__ == "__main__":
     APP.debug=True
+    APP.use_reloader=True
     APP.run()
